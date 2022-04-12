5fb26bebaa0444a9fd9f6629d70b60680f3d5e44
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-04-12 15:07:49 +0300

    с учетом рекомендаций

--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ import countryList from './templates/list.hbs';
 
 const DEBOUNCE_DELAY = 300;
 const refs = getRefs();
-let markup = '';
-let inputCountry = '';
 
 refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
 function handleInput(event) {
     // получаем данные со строки ввода
-    inputCountry = (event.target.value).trim();
+    const inputCountry = (event.target.value).trim();
     // проверяем, не пустые ли данные - пресекаем ошибочный пустой запрос
     if (inputCountry === '') { return };
     // очищаем страницу, если там что было
@@ -42,9 +40,8 @@ function renderData(data) {
 
 // очистка ранее нарисованного
 function clearField() {
-    markup = '';
-    refs.info.innerHTML = markup;
-    refs.list.innerHTML = markup;
+    refs.info.innerHTML = '';
+    refs.list.innerHTML = '';
 }
 
 // нотификация о большом количестве стран
@@ -56,19 +53,19 @@ function reportOverMax() {
 function renderCard(data) {
     let country = data[0];
     country.languages = country.languages.map(lang => lang.name).join(', ');
-    markup = countryCard(country);
-    refs.info.innerHTML = markup;
+    refs.info.innerHTML = countryCard(country);
 }
 
 // рисуем список стран 
 function renderList(data) {
-    const markupArray = data.map(element => countryList(element));
-    markupArray.forEach(element => markup += element);
+    let markup = '';
+    data
+        .map(element => countryList(element))
+        .forEach(element => markup += element);
     refs.list.insertAdjacentHTML('afterbegin', markup);
 }
 
 // корректировка нотификатора (переместил на середину с правого края)
 Notiflix.Notify.init( {
-    width: '280px',
     position: 'center-top',
 })
